7526f066cbfbe4925b9edf0d7d53981e9e7081ae
petarally <pprenc@unipu.hr>
2024-12-23 16:52:38 +0100

    knjiga prometa

--- src/viewInvoices.js
+++ src/viewInvoices.js
@@ -5,6 +5,9 @@ window.addEventListener("DOMContentLoaded", () => {
   const invoicesDataPath = path.join(__dirname, "data", "invoices.json");
   const invoicesData = JSON.parse(fs.readFileSync(invoicesDataPath));
 
+  const companyDataPath = path.join(__dirname, "data", "companyData.json");
+  const companyData = JSON.parse(fs.readFileSync(companyDataPath));
+
   const invoicesListDiv = document.getElementById("invoices-list");
 
   const formatDate = (dateString) => {
@@ -28,6 +31,98 @@ window.addEventListener("DOMContentLoaded", () => {
       <hr />
     `;
     invoicesListDiv.appendChild(invoiceDiv);
+  });
+
+  // CSV export - KNJIGA PROMETA
+  document.getElementById("export-csv").addEventListener("click", function () {
+    const selectedMonth = document.getElementById("month-select").value;
+    const selectedYear = document.getElementById("year-select").value;
+
+    // Create CSV content
+    let csvContent = "\ufeff";
+    csvContent += `;;;;;;
+;;;;;;
+Obrazac KPR;;;;;;
+KNJIGA PROMETA ${selectedYear}.;;;;;;
+I. OPĆI PODACI O POREZNO, OBVEZNIKU;;;;;;
+1. NAZIV DJELATNOSTI;;;${companyData.opis};;${companyData.kod_djelatnosti};ŠIFRA DJELATNOSTI
+2. IME I PREZIME PODUZETNIKA / NOSITELJA ZAJEDNIČKE DJELATNOSTI;;;;${companyData.vlasnik};;
+3. ADRESA PREBIVALIŠTA / UOBIČAJENOG BORAVIŠTA;;;;${companyData.adresa}, ${companyData.grad};;
+4. OIB PODUZETNIKA / NOSITELJA ZAJEDNIČKE DJELATNOSTI;;;;${companyData.oib};;
+II.PODACI O POSLOVNOJ JEDINICI;;;;;;
+1. NAZIV DJELATNOSTI;;${companyData.opis};;;;
+2. ADRESA;;${companyData.adresa}, ${companyData.grad};;;;
+RED.  BR.;NADNEVAK;BROJ                                       TEMELJNICE;OPIS ISPRAVA O PRIMICIMA                        U GOTOVINI;IZNOS NAPLAĆEN                             U GOTOVINI/ČEKOVIMA;IZNOS NAPLAĆEN                   BEZGOTOVINSKIM PUTEM(1);UKUPNO NAPLAĆEN IZNOS
+1;2;3;4;5;6;7 (5+6)`;
+
+    const filteredInvoices = invoicesData.filter((invoice) => {
+      const invoiceDate = new Date(invoice.date);
+      return (
+        invoiceDate.getMonth() + 1 === parseInt(selectedMonth) &&
+        invoiceDate.getFullYear() === parseInt(selectedYear)
+      );
+    });
+
+    // Sort by date
+    filteredInvoices.sort((a, b) => new Date(a.date) - new Date(b.date));
+    let sum = 0;
+    filteredInvoices.forEach((invoice, index) => {
+      csvContent += `${index + 1};${formatDate(invoice.date)};${
+        invoice.number
+      };;;${invoice.discountedAmount};${invoice.discountedAmount}`;
+      sum += invoice.discountedAmount;
+    });
+    csvContent += `ZBROJ;;;;0,00;${sum};${sum}`;
+
+    csvContent += ` 
+;;;;;;
+1 Popunjava porezni obveznik koji porez na dohodak od samostalne djelatnosti plaća u paušalnom iznosu u skladu s Pravilnikom o paušalnom oporezivanju samostalnih djelatnosti;;;;;;
+;;;;;;
+;;;;;;
+;;;;;;
+;;;;;;
+;;;;;;
+;;;;;;
+;;;;;;
+;;;;;;
+;;;;;;
+;;;;;;
+;;;;;;
+;;;;;;
+;;;;;;
+;;;;;;
+;;;;;;
+;;;;;;
+;;;;;;
+;;;;;;
+;;;;;;
+;;;;;;
+;;;;;;
+;;;;;;
+;;;;;;
+;;;;;;
+;;;;;;
+;;;;;;
+;;;;;;
+;;;;;;
+;;;;;;
+;;;;;;
+;;;;;;
+;;;;;;
+;;;;;;
+`;
+
+    // Trigger download
+    const blob = new Blob([csvContent], { type: "text/csv;charset=utf-8;" });
+    const link = document.createElement("a");
+    const fileName = `knjiga-prometa-${selectedMonth}-${selectedYear}.csv`;
+
+    link.href = URL.createObjectURL(blob);
+    link.download = fileName;
+    link.style.display = "none";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
   });
 
   document.getElementById("back-button").addEventListener("click", () => {
