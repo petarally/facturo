4f5f6f7ac56ce3baf3335bc3b9c6116dd2385105
petarally <pprenc@unipu.hr>
2024-12-23 17:11:22 +0100

    ostala je još statistika za napraviti2

--- src/hello.js
+++ src/hello.js
@@ -37,6 +37,12 @@ window.addEventListener("DOMContentLoaded", () => {
       window.location = "viewInvoices.html";
     });
 
+  document
+    .getElementById("view-statistics-button")
+    .addEventListener("click", () => {
+      window.location = "viewStatistics.html";
+    });
+
   document.getElementById("back-button").addEventListener("click", () => {
     window.history.back();
   });
---
+++ src/viewStatistics.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const path = require("path");
+
+window.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("back-button").addEventListener("click", () => {
+    window.location.href = "hello.html";
+  });
+});
